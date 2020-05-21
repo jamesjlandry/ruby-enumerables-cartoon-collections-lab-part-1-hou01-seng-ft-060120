2e8373ff2d89a8625d1928b27b96b3326c512619
James Landry <james.j.landry@outlook.com>
2020-05-21 16:32:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,8 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |names, index|
-    puts index + 1, names
+    index = index + 1
+    puts index , names
   end
     
 end
